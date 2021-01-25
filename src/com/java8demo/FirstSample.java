13eb5b3e0c9cc972105e4cfe5aa8bc6523e72cc2
Nehallearn <learn5151@gmail.com>
2021-01-25 19:01:21 +0000

    Second Commit via UI

--- src/com/java8demo/FirstSample.java
+++ src/com/java8demo/FirstSample.java
@@ -5,6 +5,8 @@ public class FirstSample {
     public static void main(String[] args) {
         System.out.println("hello 2");
         System.out.println("Hello");
+        System.out.println("Update hello");
+        System.out.println("New hello");
     }
 
 
