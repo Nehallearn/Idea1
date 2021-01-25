5f45ca56cc463c669120d5b4fa6b85732c38a408
Nehallearn <learn5151@gmail.com>
2021-01-25 18:42:20 +0000

    First Commit via UI

---
+++ src/com/java8demo/FirstSample.java
@@ -0,0 +1,12 @@
+package com.java8demo;
+
+public class FirstSample {
+
+    public static void main(String[] args) {
+        System.out.println("hello 2");
+        System.out.println("Hello");
+    }
+
+
+
+}
